8955d2a6ae420b586ec3e2bcacf1bd551b2811a8
Cédric Belin <cedx@outlook.com>
2026-05-22 13:53:22 +0200

    Merge some files

--- Sources/FunctionCoverage.cs
+++ Sources/FunctionCoverage.cs
@@ -30,3 +30,21 @@ public sealed class FunctionCoverage {
 		$"{Tokens.FunctionsHit}:{Hit}"
 	]);
 }
+
+/// <summary>
+/// Provides details for function coverage.
+/// </summary>
+/// <param name="FunctionName">The function name.</param>
+/// <param name="LineNumber">The line number of the function start.</param>
+/// <param name="ExecutionCount">The execution count.</param>
+public sealed record FunctionData(string FunctionName = "", int LineNumber = 0, int ExecutionCount = 0) {
+
+	/// <summary>
+	/// Returns a string representation of this object.
+	/// </summary>
+	/// <returns>The string representation of this object.</returns>
+	public override string ToString() => string.Join('\n', [
+		$"{Tokens.FunctionName}:{LineNumber},{FunctionName}",
+		$"{Tokens.FunctionData}:{ExecutionCount},{FunctionName}"
+	]);
+}
--- Tests/FunctionCoverage.Tests.cs
+++ Tests/FunctionCoverage.Tests.cs
@@ -15,3 +15,17 @@ public sealed class FunctionCoverageTests {
 		AreEqual("FN:127,main\nFNDA:3,main\nFNF:23\nFNH:11", new FunctionCoverage { Data = [data], Found = 23, Hit = 11 }.ToString());
 	}
 }
+
+/// <summary>
+/// Tests the features of the <see cref="FunctionData"/> class.
+/// </summary>
+[TestClass]
+public sealed class FunctionDataTests {
+
+	[TestMethod, DisplayName("ToString")]
+	public void TestToString() {
+		var data = new FunctionData { ExecutionCount = 3, FunctionName = "main", LineNumber = 127 };
+		AreEqual("FN:0,\nFNDA:0,", new FunctionData().ToString());
+		AreEqual("FN:127,main\nFNDA:3,main", data.ToString());
+	}
+}
